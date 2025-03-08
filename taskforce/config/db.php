0d87a37813345979d50a714350ab2c86120d0af4
Надежда <timakovanadezda0@gmail.com>
2025-03-08 21:49:43 +0300

    Настройка подключения к БД

--- taskforce/config/db.php
+++ taskforce/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=MySQL-5.7;dbname=task-force',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
