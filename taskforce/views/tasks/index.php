642161538145f33ba64aa7611bf7287ca6162029
Надежда <timakovanadezda0@gmail.com>
2025-03-12 21:30:03 +0300

    Отрисовка шаблона, добавление Active Form для отображения формы

--- taskforce/views/tasks/index.php
+++ taskforce/views/tasks/index.php
@@ -2,38 +2,42 @@
 
 /** 
  * @var $this yii\web\View 
- * @var Task[] $task
+ * @var Task[] $models
+ * @var Task $task
+ * @var Category[] $categories
  */
 
 use yii\helpers\Html;
-use yii\web\View;
+use yii\widgets\ActiveForm;
 
 $this->title = 'Новые задания';
 ?>
+
 <main class="main-content container">
     <div class="left-column">
+
         <h3 class="head-main head-task">Новые задания</h3>
         <?php
-        if ($tasks):
-            foreach ($tasks as $task): ?>
+        if ($models):
+            foreach ($models as $model): ?>
                 <div class="task-card">
                     <div class="header-task">
-                        <a href="#" class="link link--block link--big"><?= Html::encode($task->task); ?></a>
-                        <?php if ($task->price): ?>
-                            <p class="price price--task"><?= $task->price; ?> ₽</p>
+                        <a href="#" class="link link--block link--big"><?= Html::encode($model->task); ?></a>
+                        <?php if ($model->price): ?>
+                            <p class="price price--task"><?= $model->price; ?> ₽</p>
                         <?php endif; ?>
                     </div>
-                    <p class="info-text"><span class="current-time"><?= Yii::$app->formatter->asRelativeTime($task->date_public); ?></span></p>
-                    <p class="task-text"><?= Html::encode($task->discription); ?></p>
+                    <p class="info-text"><span class="current-time">
+                            <?= Yii::$app->formatter->asRelativeTime($model->date_public); ?>
+                        </span></p>
+                    <p class="task-text"><?= Html::encode($model->discription); ?></p>
                     <div class="footer-task">
-                        <?php if ($task->id_address): ?>
-                            <p class="info-text town-text"><?php $task->address; ?></p>
+                        <?php if ($model->id_address): ?>
+                            <p class="info-text town-text"><?php $model->address; ?></p>
                         <?php endif; ?>
-                        <?php foreach ($task->categories as $category): ?>
+                        <?php foreach ($model->categories as $category): ?>
                             <p class="info-text category-text"><?= $category->name_category; ?></p>
                         <?php endforeach; ?>
-
-
                         <a href="#" class="button button--black">Смотреть Задание</a>
                     </div>
                 </div>
@@ -65,11 +69,20 @@ $this->title = 'Новые задания';
     <div class="right-column">
         <div class="right-card black">
             <div class="search-form">
-                <form>
-                    <h4 class="head-card">Категории</h4>
-                    <div class="form-group">
-                        <div class="checkbox-wrapper">
-                            <label class="control-label" for="сourier-services">
+                <?php $form = ActiveForm::begin(); ?>
+                <h4 class="head-card">Категории</h4>
+
+                <div class="checkbox-wrapper">
+                    <?= Html::activeCheckboxList(
+                        $task,
+                        'id_category',
+                        array_column($categories, 'name_category', 'id_category'),
+                        [
+                            'tag' => NULL,
+                            'itemOptions' => ['labelOptions' => ['class' => 'control-label']]
+                        ]
+                    ) ?>
+                    <!-- <label class="control-label" for="сourier-services">
                                 <input type="checkbox" id="сourier-services" checked>
                                 Курьерские услуги</label>
                             <label class="control-label" for="cargo-transportation">
@@ -77,26 +90,32 @@ $this->title = 'Новые задания';
                                 Грузоперевозки</label>
                             <label class="control-label" for="translations">
                                 <input id="translations" type="checkbox">
-                                Переводы</label>
-                        </div>
-                    </div>
-                    <h4 class="head-card">Дополнительно</h4>
-                    <div class="form-group">
-                        <label class="control-label" for="without-performer">
+                                Переводы</label> -->
+                </div>
+                <h4 class="head-card">Дополнительно</h4>
+                <?= $form->field($task, 'is_without_executor')
+                    ->checkbox(['labelOptions' => ['class' => 'control-label']]); ?>
+                <?= $form->field($task, 'is_without_location')
+                    ->checkbox(['labelOptions' => ['class' => 'control-label']]); ?>
+                <!-- <label class="control-label" for="without-performer">
                             <input id="without-performer" type="checkbox" checked>
-                            Без исполнителя</label>
-                    </div>
-                    <h4 class="head-card">Период</h4>
-                    <div class="form-group">
-                        <label for="period-value"></label>
-                        <select id="period-value">
-                            <option>1 час</option>
-                            <option>12 часов</option>
-                            <option>24 часа</option>
-                        </select>
-                    </div>
-                    <input type="submit" class="button button--blue" value="Искать">
-                </form>
+                            Без исполнителя
+                        </label> 
+                        <label class="control-label" for="without-location">
+                            <input id="without-location" type="checkbox" checked>
+                            Без исполнителя
+                        </label> 
+                        -->
+                <h4 class="head-card">Период</h4>
+                <?=
+                $form->field($task, 'filter_period', ['template' => '{input}'])->dropDownList([
+                    3600 => 'За последний час',
+                    86400 => 'За сутки',
+                    604800 => 'За неделю'
+                ], ['prompt' => 'Выбрать']);
+                ?>
+                <input type="submit" class="button button--blue" value="Искать">
+                <?php ActiveForm::end(); ?>
             </div>
         </div>
     </div>
